a0135e10f0642a553c630b3aacd42255ee8a339c
shubham <shubh.9@outlook.com>
2020-06-21 16:08:08 +0530

    Created couple of API's and database connection

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 const route = require('./api/index');
 const auth = require('./api/auth');
-const test = require('./api/test');
+const farmer = require('./api/farmer');
 
 app.use(express.static('public'))
 
 // Routes
 app.use('/', route);
 app.use('/auth', auth);
-app.use('/test', test);
+app.use('/farmer', farmer);
+
+// in development we are using a self signed certificate for SSL communication.
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 async function startServer() {
     app.listen(4000, err => {
---
+++ src/server/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pgConfig: {
+        host: 'ec2-34-198-243-120.compute-1.amazonaws.com',
+        user: 'nxmwzpiqergtic',
+        password: '3d7455404d179f7f5918ace02cd8128bd851fedcbcf6679b8903b4eb467190cb',
+        database: 'd600sqm9b1nmj3',
+        ssl: true,
+        schema: 'savera'
+    }
+}
