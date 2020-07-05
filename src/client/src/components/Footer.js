6a946e930d6ff5f9ae6da96f8b10c80a1d272fae
meghnaY <meghnayadava2909@gmail.com>
2020-07-05 19:30:49 +0530

    Add Boostrap, Navbar, Footer

---
+++ src/client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div class="w-100 p-3 bg-dark text-white text-center fixed-bottom">NGO-SWERA</div>
+    )
+}
+
+export default Footer;
