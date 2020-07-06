cb468ff9d3efaa894637d2decadf6e1e651efd61
Pankaj Bhatia <bhatia.pankaj92@gmail.com>
2020-07-06 19:41:54 +0530

    adding endpoints for dissemination and question service

---
+++ src/server/services/disseminationService.js
@@ -0,0 +1,19 @@
+exports.getAllDisseminations = (req, res, next) => {
+    //returns all the Disseminations
+};
+
+exports.saveDissemination = (req, res, next) => {
+    //Save a particular Dissemination in database
+};
+
+exports.getDisseminationById = (req, res, next) => {
+    //Gets particular Dissemination from database
+};
+
+exports.deleteDisseminationById = (req, res, next) => {
+    //Deletes particular Dissemination from database
+};
+
+exports.updateDisseminationById = (req, res, next) => {
+    //updates particular Dissemination from database
+};
