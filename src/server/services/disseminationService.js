1b31cddc57a32b3498b64f833822ddb4f603f265
mephi007 <roysumit949@gmail.com>
2020-07-22 02:36:12 +0530

    Issue-54 Read Dissemination

--- src/server/services/disseminationService.js
+++ src/server/services/disseminationService.js
@@ -1,5 +1,34 @@
+var knex = require('../utilities/createKnexPGConnection');
+const config = require('../config');
+
 exports.getAllDisseminations = (req, res, next) => {
     //returns all the Disseminations
+    knex.withSchema(config.pgConfig.schema)
+    .table('dissemination')
+    .select('dissemination.dissemination_id',
+            'dissemination.date_of_dissemination',
+            'dissemination.start_time',
+            'dissemination.end_time',
+            'dissemination.name_of_video',
+            'dissemination.place',
+            'dissemination.village_name',
+            'dissemination.gram_panchayat',
+            'farmer.name',
+            'farmer.father_name',
+            'farmer.spouse_name',
+            'adoption_table.adp_id',
+            'question_table.q_id'
+    )
+    .join('dissemination_attendee_mapping', {'dissemination.dissemination_id' : 'dissemination_attendee_mapping.dissemination_id'})
+    .join('farmer', {'farmer.farmer_id' : 'dissemination_attendee_mapping.farmer_id'})
+    .join('question_table', {'question_table.q_id' : 'dissemination_attendee_mapping.question_id'})
+    .join('adoption_table', {'adoption_table.adp_id' : 'dissemination_attendee_mapping.adoption_id'})
+    .then(function (rows) {
+        next(null, rows);
+    })
+    .catch(function (err) {
+        next(err);
+    });
 };
 
 exports.saveDissemination = (req, res, next) => {
@@ -8,6 +37,34 @@ exports.saveDissemination = (req, res, next) => {
 
 exports.getDisseminationById = (req, res, next) => {
     //Gets particular Dissemination from database
+    const id = req.id;
+    knex.withSchema(config.pgConfig.schema)
+    .table('dissemination')
+    .select('dissemination.dissemination_id',
+            'dissemination.date_of_dissemination',
+            'dissemination.start_time',
+            'dissemination.end_time',
+            'dissemination.name_of_video',
+            'dissemination.place',
+            'dissemination.village_name',
+            'dissemination.gram_panchayat',
+            'farmer.name',
+            'farmer.father_name',
+            'farmer.spouse_name',
+            'adoption_table.adp_id',
+            'question_table.q_id'
+    )
+    .join('dissemination_attendee_mapping', {'dissemination.dissemination_id' : 'dissemination_attendee_mapping.dissemination_id'})
+    .join('farmer', {'farmer.farmer_id' : 'dissemination_attendee_mapping.farmer_id'})
+    .join('question_table', {'question_table.q_id' : 'dissemination_attendee_mapping.question_id'})
+    .join('adoption_table', {'adoption_table.adp_id' : 'dissemination_attendee_mapping.adoption_id'})
+    .where({ 'dissemination.dissemination_id': parseInt(req.params.id) })
+    .then(function (rows) {
+        next(null, rows);
+    })
+    .catch(function (err) {
+        next(err);
+    });
 };
 
 exports.deleteDisseminationById = (req, res, next) => {
