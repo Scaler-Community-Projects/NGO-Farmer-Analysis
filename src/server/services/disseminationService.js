3c3f270cd21e4437ffa14c87cbb7b6ef204a770e
Pankaj Bhatia <bhatia.pankaj92@gmail.com>
2020-07-22 02:23:45 +0530

    implementing create dissemination

--- src/server/services/disseminationService.js
+++ src/server/services/disseminationService.js
@@ -1,9 +1,27 @@
+var knex = require('../utilities/createKnexPGConnection');
+const config = require('../config');
+
 exports.getAllDisseminations = (req, res, next) => {
     //returns all the Disseminations
 };
 
 exports.saveDissemination = (req, res, next) => {
-    //Save a particular Dissemination in database
+    req.body.created_date = new Date();
+    knex.transaction(function (trx) {
+        knex.withSchema(config.pgConfig.schema)
+            .table('dissemination')
+            .insert(req.body)
+            .returning('*')
+            .transacting(trx)
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
+        .then(function (rows) {
+            next(null, rows);
+        })
+        .catch(function (err) {
+            next(err);
+        });
 };
 
 exports.getDisseminationById = (req, res, next) => {
