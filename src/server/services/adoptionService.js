4dc216fa648461a81f7fb98c3cb536cecec623c6
ANIRBAN CHAKRABORTY <discoveranirban@gmail.com>
2020-08-01 02:36:47 +0530

    Implemented Read Adoption #56
    Implemented Read Adoption details by ID.

--- src/server/services/adoptionService.js
+++ src/server/services/adoptionService.js
@@ -1,8 +1,22 @@
+var knex = require('../utilities/createKnexPGConnection');
+const config = require('../config');
+
 exports.getAllAdoption = (req, res, next) => {};
 
 exports.saveAdoptionDetails = (req, res, next) => {};
 
-exports.getAdoptionById = (req, res, next) => {};
+exports.getAdoptionById = (req, res, next) => {
+    knex.withSchema(config.pgConfig.schema)
+        .table('adoption_table')
+        .select("*")
+        .where({ "adp_id": parseInt(req.params.id) })
+        .then(function (row) {
+            next(null, row)
+        })
+        .catch(function (err) {
+            next(err)
+        });
+};
 
 exports.deleteFarmerById = (req, res, next) => {};
 
